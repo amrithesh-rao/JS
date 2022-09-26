f3ff8748cf1651ed6f3460b0f8cf00b61940a81e
Amrithesh Rao <66878277+amrithesh-rao@users.noreply.github.com>
2022-09-26 17:39:27 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,18 @@
+var array = [1,2,3,4,5];
+console.log(array.includes(9))
+
+array.push(6);
+console.log(array)
+
+array.pop();
+
+console.log(array)
+array.pop(3);
+console.log(array)
+
+array.shift(2);
+console.log(array)
+array.unshift(-1);
+console.log(array)
+
+
