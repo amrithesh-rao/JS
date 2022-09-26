96859e2d5d34062f91c9ac5375237d662ca30839
Amrithesh Rao <66878277+amrithesh-rao@users.noreply.github.com>
2022-09-26 13:06:02 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,12 @@
+/* var a = 5;
+var b = 10;
+
+[a,b] = [b,a];
+console.log(a+" "+b);
+*/
+
+var name ="applE";
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.charAt(0).toUpperCase()+name.slice(1).toLowerCase());
---
+++ random.js
@@ -0,0 +1,3 @@
+var gen = Math.random()*100;
+var per = (Math.floor(gen)+1);
+console.log(per+"%");
